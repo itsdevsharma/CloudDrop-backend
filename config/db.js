7c0e3e12749b0948f5c67b774489d18c266e1a3d
itsdevsharma <137605318+itsdevsharma@users.noreply.github.com>
2025-11-20 00:27:50 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,10 @@
-import cloudinary from 'cloudinary';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import cloudinary from "cloudinary";
 
 cloudinary.v2.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_SECRET_KEY
-})
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 export default cloudinary;
+
